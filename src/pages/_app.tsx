86267171519bb99e8d9c3e47e076b126ff0a8283
Aihara <aihara-sama@mail.ru>
2023-11-08 12:40:44 +0200

    feat(styling-lib): integrated mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,28 @@
+import type { EmotionCache } from "@emotion/react";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
 import { fontFamily } from "fonts";
-import type { AppProps } from "next/app";
+import type { AppProps as NextAppProps } from "next/app";
+import createEmotionCache from "utils/createEmotionCache";
+
+const clientSideEmotionCache = createEmotionCache();
+interface AppProps extends NextAppProps {
+  emotionCache?: EmotionCache;
+}
 
 const App = (props: AppProps) => {
-  const { Component, pageProps } = props;
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <>
+    <CacheProvider value={emotionCache}>
       <style jsx global>{`
         html {
           font-family: ${fontFamily};
         }
       `}</style>
-      <Component {...pageProps} />;
-    </>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
   );
 };
 
